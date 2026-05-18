9cab8d5fdd761dd25c94841159cfe1a84db40c4e
Nitya Gandhi <nityagandhi1125@gmail.com>
2026-05-18 20:18:21 +0530

    fixed forgot controller import

--- real-estate-backend/routes/forgotPasswordRoutes.js
+++ real-estate-backend/routes/forgotPasswordRoutes.js
@@ -1,7 +1,7 @@
     const express = require("express");
     const router = express.Router();
 
-    const {handleForgotPassword, handleResetPassword} = require("../controllers/ForgotController")
+    const {handleForgotPassword, handleResetPassword} = require("../controllers/ForgotController");
 
     router.post("/forgot-password", handleForgotPassword);
     router.patch("/reset-password/:token", handleResetPassword);
